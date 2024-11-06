2c433368a8863c538ea24911381cab55a030b207
k8ANStar <kstarchuk3@my.bcit.ca>
2024-11-05 23:42:45 -0800

    updating git ignore and readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# How to start next js app
+
+- create repo
+- clone to desktop
+- cd into it
+- npx create-next-app@latest
+- name it
+- go through dependences
--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,15 @@
 import Head from "next/head";
 import DataDisplay from "./components/molecules/DataDisplay";
+//implementaton
+// pick an api X
+// build a button component that has a fetch action/clear action X
+// build a component thats displays our data (should have an empty and fulfilled state) X
+// build a function that will fetch some data X
+// format and handle the data
+// (error handling do on own)
+// style our app and create breakpoints
+//component for our button to sit in
+// clean code
 
 export default function Home() {
 	return (
